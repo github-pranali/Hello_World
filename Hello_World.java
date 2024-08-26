41d701dbb4379f51bb2f47afd3677090667446e2
github-pranali <pranali2905patil@gmail.com>
2024-08-26 20:00:39 +0530

    Update Hello_World.java

--- Hello_World.java
+++ Hello_World.java
@@ -1,6 +1,6 @@
 public class Hello_World {
     public static void main(String[] args){
-        System.out.println("Welcome to java..");
+        System.out.println("Hello, World!");
     }
     
 }
